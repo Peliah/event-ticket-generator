20297b1e482044e447d31c22a9d1a04d600c2bba
Peliah <pelepoupa@gmail.com>
2025-02-14 03:40:54 +0100

    FEAT: added a barcode generated feature

--- components/FormContainer/Step3.tsx
+++ components/FormContainer/Step3.tsx
@@ -3,6 +3,9 @@ import { StepProps } from '@/utils/types';
 import Image from 'next/image';
 
 import { toPng } from 'html-to-image';
+import { useBarcode } from 'next-barcode';
+
+
 const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
 
     const ref = useRef<HTMLDivElement>(null)
@@ -25,6 +28,16 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
             })
     }, [ref]);
 
+
+    const { inputRef } = useBarcode({
+        value: 'localhost:3000',
+        options: {
+            background: 'transparent',
+            lineColor: '#FFFFFF',
+            displayValue: false,
+        }
+    });
+
     return (
         <div className='text-center gap-8'>
             <div className='py-8 '>
@@ -34,7 +47,7 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
 
             <div className='py-8 px-5' ref={ref}>
                 <div className='flex items-center justify-center'>
-                    <div className="ticket border border-[#24A0B5] ">
+                    <div className="ticket border border-[#24A0B5] bg-[#072C31]">
                         <div className="ticket--start">
                             <div className='m-5 border border-[#24A0B5] rounded-2xl p-[14px] bg-[rgba(3, 30, 33, 0.1)] backdrop:blur-sm'>
                                 <div className='flex flex-col gap-5 items-center justify-center'>
@@ -43,12 +56,14 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
                                         <h1 className='font-[family-name:var(--font-road-rage)] font-normal text-[34px] leading-[34px]'>
                                             Techember Fest ”25
                                         </h1>
-                                        <p className='font-[family-name:var(--font-roboto)] font-normal text-[10px] leading-[15px]'>
-                                            📍 04 Rumens road, Ikoyi, Lagos
-                                        </p>
-                                        <p className='font-[family-name:var(--font-roboto)] font-normal text-[10px] leading-[15px]'>
-                                            📅 March 15, 2025 | 7:00 PM
-                                        </p>
+                                        <div className='p-1 flex flex-col gap-1'>
+                                            <p className='font-[family-name:var(--font-roboto)] font-normal text-[10px] leading-[15px]'>
+                                                📍 04 Rumens road, Ikoyi, Lagos
+                                            </p>
+                                            <p className='font-[family-name:var(--font-roboto)] font-normal text-[10px] leading-[15px]'>
+                                                📅 March 15, 2025 | 7:00 PM
+                                            </p>
+                                        </div>
                                     </div>
 
                                     <div className='border-4 rounded-xl w-[140px] h-[140px] border-[rgba(36, 160, 181, 0.5)]'>
@@ -101,7 +116,7 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
                         }}>
                         </div>
                         <div className="ticket--end">
-
+                            <img ref={inputRef} />
                         </div>
                     </div>
                 </div>
