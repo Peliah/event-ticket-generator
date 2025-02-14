c3c3e58fc098ac96ec9f69a71e577f1a4fbae534
Peliah <pelepoupa@gmail.com>
2025-02-14 02:38:40 +0100

    Update: Added download button

--- components/FormContainer/Step3.tsx
+++ components/FormContainer/Step3.tsx
@@ -2,9 +2,14 @@ import { StepProps } from '@/utils/types';
 import Image from 'next/image';
 
 const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
+
+    const handleDownloadTicket = () => {
+        alert('Download functionality will be implemented here.');
+    };
+
     return (
         <div className='text-center gap-8'>
-            <div className='pt-8 '>
+            <div className='py-8 '>
                 <h1 className='text-white text-[32px] font-normal font-[family-name:var(--font-alatsi)]'>Your Ticket is Booked!</h1>
                 <p className='text-neutral-50 text-base font-normal font-[family-name:var(--font-roboto)] leading-normal'>Check your email for a copy or you can <span>download</span></p>
             </div>
@@ -31,7 +36,7 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
                                     <div className='border-4 rounded-xl w-[140px] h-[140px] border-[rgba(36, 160, 181, 0.5)]'>
                                         <Image
                                             src={formData.avatar}
-                                            alt="Preview"
+                                            alt="avatar"
                                             sizes="100vw"
                                             height={10}
                                             width={10}
@@ -84,15 +89,16 @@ const Step3: React.FC<StepProps> = ({ prevStep, formData }) => {
                 </div>
             </div>
 
-            <div className='flex gap-6 w-full text-base font-light' style={{ fontFamily: 'Jeju Myeongjo, serif' }}>
+            <div className='flex gap-6 w-full text-base font-light pt-6' style={{ fontFamily: 'Jeju Myeongjo, serif' }}>
                 <button
                     className='rounded-lg px-6 py-3 w-1/2 text-center border border-[#24A0B5] text-[#24A0B5]'
                     onClick={prevStep}
                 >
-                    Book Another Ticked
+                    Book Another Ticket
                 </button>
                 <button
                     className='rounded-lg px-6 py-3 w-1/2 text-center bg-[#24A0B5] text-white'
+                    onClick={handleDownloadTicket}
                 >
                     Download Ticket
                 </button>
